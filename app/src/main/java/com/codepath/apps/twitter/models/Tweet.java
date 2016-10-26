30495feb90eda4bb33ab2b67440b06788d6e729c
sraveesh <sraveesh@ebay.com>
2016-10-25 18:50:36 -0700

    init

---
+++ app/src/main/java/com/codepath/apps/twitter/models/Tweet.java
@@ -0,0 +1,61 @@
+package com.codepath.apps.twitter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Tweet {
+
+    private String body;
+    private long uid;
+    private String createdAt;
+    private User user;
+
+    public String getBody() {
+        return body;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public static Tweet fromJSON(JSONObject jsonObject) {
+        Tweet tweet = new Tweet();
+        try {
+            tweet.body = jsonObject.getString("text");
+            tweet.uid = jsonObject.getLong("id");
+            tweet.createdAt = jsonObject.getString("created_at");
+            tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return tweet;
+    }
+
+    public static ArrayList<Tweet> fromJSONArray(JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject tweetJson = jsonArray.getJSONObject(i);
+                Tweet tweet = fromJSON(tweetJson);
+                if(tweet != null) {
+                    tweets.add(tweet);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        return tweets;
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/twitter/models/User.java
@@ -0,0 +1,42 @@
+package com.codepath.apps.twitter.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+
+    private String name;
+    private Long uid;
+    private String screenName;
+    private String profileImageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public static User fromJSON(JSONObject jsonObject) {
+        User user = new User();
+        try {
+            user.name = jsonObject.getString("name");
+            user.uid = jsonObject.getLong("id");
+            user.screenName = jsonObject.getString("screen_name");
+            user.profileImageUrl = jsonObject.getString("profile_image_url");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+}
